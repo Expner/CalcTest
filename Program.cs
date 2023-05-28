8c128f3e5f9b9c9c312b5b5051467c9abc34d49f
Expner <KDS4wexp@gmail.com>
2023-05-28 18:40:50 +0300

    переименовал переменные и массивы

--- Program.cs
+++ Program.cs
@@ -1,64 +1,68 @@
-﻿string example = "";
-example = Console.ReadLine();
-string[] ex = example.Split(new char[] { ' ' });
-int q = 0, // +
-    w = 0, // -
-    e = 0, // *
-    r = 0; // /
-foreach (char s in example)
+﻿string expression = Console.ReadLine();
+string[] massive_Char_expression = expression.Split(new char[] { ' ' });
+string[] expression_after = new string[massive_Char_expression.Length];
+
+int var_plus = 0, // +
+    var_minus = 0, // -
+    var_mult = 0, // *
+    var_div = 0; // /
+
+foreach (char s in expression) // перебираем выражение и считаем знаки в строке
 {
-    if (s == '+') q++;
-    if (s == '-') w++;
-    if (s == '*') e++;
-    if (s == '/') r++;
+    if (s == '+') var_plus++;
+    if (s == '-') var_minus++;
+    if (s == '*') var_mult++;
+    if (s == '/') var_div++;
 }
-string p = "";
-string p0 = new string('*', e);
-string p1 = new string('/', r);
-string p2 = new string('+', q);
-string p3 = new string('-', w);
-p = p0 + p1 + p2 + p3;
-//God is ashamed of me.... sorted char by hierarchy
-string[] w1 = new string[ex.Length];
-foreach (char s in p)
+
+string str_all = ""; // создаем строку в которой определим знаки из примера по иерархии
+string str_mult = new string('*', var_mult);
+string str_div = new string('/', var_div);
+string str_plus = new string('+', var_plus);
+string str_minus = new string('-', var_minus);
+str_all = str_mult + str_div + str_plus + str_minus;
+
+foreach (char s in str_all) 
 {
     switch (s)
     {
         case '*':
-            Array.Resize(ref w1, ex.Length - 2);
-            Array.Copy(ex, 0, w1, 0, Array.IndexOf(ex, "*") - 1);
-            w1[Array.IndexOf(ex, "*") - 1] = Convert.ToString(Convert.ToInt32(ex[Array.IndexOf(ex, "*") - 1]) * Convert.ToInt32(ex[Array.IndexOf(ex, "*") + 1]));
-            Array.Copy(ex, Array.IndexOf(ex, "*") + 2, w1, Array.IndexOf(ex, "*"), w1.Length - Array.IndexOf(ex, "*"));
-            Array.Resize(ref ex, ex.Length - 2);
-            Array.Copy(w1, 0, ex, 0, w1.Length);
+            Array.Resize(ref expression_after, massive_Char_expression.Length - 2);
+            Array.Copy(massive_Char_expression, 0, expression_after, 0, Array.IndexOf(massive_Char_expression, "*") - 1);
+            expression_after[Array.IndexOf(massive_Char_expression, "*") - 1] = Convert.ToString(Convert.ToInt32(massive_Char_expression[Array.IndexOf(massive_Char_expression, "*") - 1]) * Convert.ToInt32(massive_Char_expression[Array.IndexOf(massive_Char_expression, "*") + 1]));
+            Array.Copy(massive_Char_expression, Array.IndexOf(massive_Char_expression, "*") + 2, expression_after, Array.IndexOf(massive_Char_expression, "*"), expression_after.Length - Array.IndexOf(massive_Char_expression, "*"));
+            Array.Resize(ref massive_Char_expression, massive_Char_expression.Length - 2);
+            Array.Copy(expression_after, 0, massive_Char_expression, 0, expression_after.Length);
             break;
 
         case '/':
-            Array.Resize(ref w1, ex.Length - 2);
-            Array.Copy(ex, 0, w1, 0, Array.IndexOf(ex, "/") - 1);
-            w1[Array.IndexOf(ex, "/") - 1] = Convert.ToString(Convert.ToInt32(ex[Array.IndexOf(ex, "/") - 1]) / Convert.ToInt32(ex[Array.IndexOf(ex, "/") + 1]));
-            Array.Copy(ex, Array.IndexOf(ex, "/") + 2, w1, Array.IndexOf(ex, "/"), w1.Length - Array.IndexOf(ex, "/"));
-            Array.Resize(ref ex, ex.Length - 2);
-            Array.Copy(w1, 0, ex, 0, w1.Length);
+            Array.Resize(ref expression_after, massive_Char_expression.Length - 2);
+            Array.Copy(massive_Char_expression, 0, expression_after, 0, Array.IndexOf(massive_Char_expression, "/") - 1);
+            expression_after[Array.IndexOf(massive_Char_expression, "/") - 1] = Convert.ToString(Convert.ToInt32(massive_Char_expression[Array.IndexOf(massive_Char_expression, "/") - 1]) / Convert.ToInt32(massive_Char_expression[Array.IndexOf(massive_Char_expression, "/") + 1]));
+            Array.Copy(massive_Char_expression, Array.IndexOf(massive_Char_expression, "/") + 2, expression_after, Array.IndexOf(massive_Char_expression, "/"), expression_after.Length - Array.IndexOf(massive_Char_expression, "/"));
+            Array.Resize(ref massive_Char_expression, massive_Char_expression.Length - 2);
+            Array.Copy(expression_after, 0, massive_Char_expression, 0, expression_after.Length);
             break;
 
         case '+':
-            Array.Resize(ref w1, ex.Length - 2);
-            Array.Copy(ex, 0, w1, 0, Array.IndexOf(ex, "+") - 1);
-            w1[Array.IndexOf(ex, "+") - 1] = Convert.ToString(Convert.ToInt32(ex[Array.IndexOf(ex, "+") - 1]) + Convert.ToInt32(ex[Array.IndexOf(ex, "+") + 1]));
-            Array.Copy(ex, Array.IndexOf(ex, "+") + 2, w1, Array.IndexOf(ex, "+"), w1.Length - Array.IndexOf(ex, "+"));
-            Array.Resize(ref ex, ex.Length - 2);
-            Array.Copy(w1, 0, ex, 0, w1.Length);
+            Array.Resize(ref expression_after, massive_Char_expression.Length - 2);
+            Array.Copy(massive_Char_expression, 0, expression_after, 0, Array.IndexOf(massive_Char_expression, "+") - 1);
+            expression_after[Array.IndexOf(massive_Char_expression, "+") - 1] = Convert.ToString(Convert.ToInt32(massive_Char_expression[Array.IndexOf(massive_Char_expression, "+") - 1]) + Convert.ToInt32(massive_Char_expression[Array.IndexOf(massive_Char_expression, "+") + 1]));
+            Array.Copy(massive_Char_expression, Array.IndexOf(massive_Char_expression, "+") + 2, expression_after, Array.IndexOf(massive_Char_expression, "+"), expression_after.Length - Array.IndexOf(massive_Char_expression, "+"));
+            Array.Resize(ref massive_Char_expression, massive_Char_expression.Length - 2);
+            Array.Copy(expression_after, 0, massive_Char_expression, 0, expression_after.Length);
+            break;
             break;
 
         case '-':
-            Array.Resize(ref w1, ex.Length - 2);
-            Array.Copy(ex, 0, w1, 0, Array.IndexOf(ex, "-") - 1);
-            w1[Array.IndexOf(ex, "-") - 1] = Convert.ToString(Convert.ToInt32(ex[Array.IndexOf(ex, "-") - 1]) - Convert.ToInt32(ex[Array.IndexOf(ex, "-") + 1]));
-            Array.Copy(ex, Array.IndexOf(ex, "-") + 2, w1, Array.IndexOf(ex, "-"), w1.Length - Array.IndexOf(ex, "-"));
-            Array.Resize(ref ex, ex.Length - 2);
-            Array.Copy(w1, 0, ex, 0, w1.Length);
+            Array.Resize(ref expression_after, massive_Char_expression.Length - 2);
+            Array.Copy(massive_Char_expression, 0, expression_after, 0, Array.IndexOf(massive_Char_expression, "-") - 1);
+            expression_after[Array.IndexOf(massive_Char_expression, "-") - 1] = Convert.ToString(Convert.ToInt32(massive_Char_expression[Array.IndexOf(massive_Char_expression, "-") - 1]) - Convert.ToInt32(massive_Char_expression[Array.IndexOf(massive_Char_expression, "-") + 1]));
+            Array.Copy(massive_Char_expression, Array.IndexOf(massive_Char_expression, "-") + 2, expression_after, Array.IndexOf(massive_Char_expression, "-"), expression_after.Length - Array.IndexOf(massive_Char_expression, "-"));
+            Array.Resize(ref massive_Char_expression, massive_Char_expression.Length - 2);
+            Array.Copy(expression_after, 0, massive_Char_expression, 0, expression_after.Length);
+            break;
             break;
     }
 }
-Console.WriteLine(w1[0]);
+Console.WriteLine(expression_after[0]);
